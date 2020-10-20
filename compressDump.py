4a098e9c521a4e663a7623f2ecacd425251d96d5
csprh <paul.hill@bristol.ac.uk>
2020-10-20 20:21:46 +0100

    update

--- compressDump.py
+++ compressDump.py
@@ -186,7 +186,7 @@ def compress_and_decompress(args, iNum):
                 thisLat0 = latOutPre.numpy(); thisLat0 = thisLat0[subidx,:,:,:]
                 thisHyp1 = hyperOutPost.numpy(); thisHyp1 = thisHyp1[subidx,:,:,:]
                 thisLat1 = latOutPost.numpy(); thisLat1 = thisLat1[subidx,:,:,:]
-                np.savez(fnameNPX,lat0=thisLat0, lat1=thisLat1, hyp1=thisHyp1, thisCat = args.thisCat)
+                np.savez(fnameNPX,lat0=thisLat0, lat1=thisLat1, hyp1=thisHyp1, thisCat = to_categorical(args.thisCat,101))
                 #torchvision.utils.save_image(reconstruction[subidx], fname, normalize=True)
                 output_filenames_total.append(fname)
                 iNum = iNum+1
