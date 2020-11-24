b72ae133b31f62679afaaf78f99a7e27670d047d
csprh <paul.hill@bristol.ac.uk>
2020-11-24 11:35:06 +0000

    update

--- train_C101.py
+++ train_C101.py
@@ -312,11 +312,11 @@ if __name__ == '__main__':
     logger.info('USING DEVICE {}'.format(device))
     logger.info('USING GPU ID {}'.format(args.gpu))
     logger.info('USING DATASET: {}'.format(args.dataset))
-    C101Root = './data'
+    C101Root = '/space/csprh/DASA/DATABASES/'
 
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    trainset = torchvision.datasets.Caltech101(root=C101Root, train=True,
+    trainset = torchvision.datasets.Caltech101(root=C101Root,
                                         download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                           shuffle=True, num_workers=2)
