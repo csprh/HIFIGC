4178b369c995b29f46de3042d1ecf8c60f8b5167
csprh <paul.hill@bristol.ac.uk>
2020-12-06 19:40:19 +0000

    update

--- train_C101.py
+++ train_C101.py
@@ -110,7 +110,8 @@ def end_of_epoch_metrics(args, model, data_loader, device, logger):
           B = dataAll.size(0)
           for idxB in range(B):
             data = dataAll[idxB,:,:,:]
-            y = yAll[idxB,:,:,:]
+            data = data.unsqueeze(0)
+            y = yAll[idxB]
             model.set_model_mode(old_mode)
             model.training = False
             losses = model(data, y, train_generator=False)
